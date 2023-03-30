2af4f3a1f29117eb905825abd85f336bddf43d28
MirunaGeorgescu <miruna.georgescu1@s.unibuc.ro>
2023-03-30 15:30:03 +0300

    Updated class: Catalog

--- src/main/java/ro/pao/model/Library.java
+++ src/main/java/ro/pao/model/Library.java
@@ -4,5 +4,9 @@ import ro.pao.model.abstracts.AbstractEntity;
 
 public class Library extends AbstractEntity {
     // inherits from AbstractEntity: id, createdDate, updatedDate, deletedDate
-
+    private String name;
+    private String address;
+    private String phoneNumber;
+    private String email;
+    private Catalog catalog;
 }
