88149acc1813b32df68f2f0c087bc936eaf31c9a
MirunaGeorgescu <miruna.georgescu1@s.unibuc.ro>
2023-03-30 13:07:12 +0300

    Added class: Account
    Added enum: AccountStatus

---
+++ src/main/java/ro/pao/model/abstracts/Account.java
@@ -0,0 +1,7 @@
+package ro.pao.model.abstracts;
+
+public abstract class Account extends Person {
+    private String userName;
+    private String password;
+
+}
--- src/main/java/ro/pao/model/abstracts/Person.java
+++ src/main/java/ro/pao/model/abstracts/Person.java
@@ -1,5 +1,20 @@
 package ro.pao.model.abstracts;
 
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public abstract class Person extends AbstractEntity {
+    private String name;
+    private String address;
+    private String email;
+    private String phoneNumber;
 
 }
