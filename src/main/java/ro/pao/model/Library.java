7f51da481960b92c147bef2dcc36e85e1ab01fd3
MirunaGeorgescu <miruna.georgescu1@s.unibuc.ro>
2023-03-30 15:22:45 +0300

    Added classes: Library, Catalog

---
+++ src/main/java/ro/pao/model/Library.java
@@ -0,0 +1,8 @@
+package ro.pao.model;
+
+import ro.pao.model.abstracts.AbstractEntity;
+
+public class Library extends AbstractEntity {
+    // inherits from AbstractEntity: id, createdDate, updatedDate, deletedDate
+
+}
