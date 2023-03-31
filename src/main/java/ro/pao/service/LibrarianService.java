2d387b56e9055f70be62bd9061226501324385ee
MirunaGeorgescu <miruna.georgescu1@s.unibuc.ro>
2023-03-31 12:42:44 +0300

    Implemented methods for LibrarianService: getLibrarianById, getLibrarianByUserName, getAllLibrarians, addLibrarian, addLibrarians, deleteLibrarianById, deleteLibrarianByUserName

--- src/main/java/ro/pao/service/LibrarianService.java
+++ src/main/java/ro/pao/service/LibrarianService.java
@@ -1,5 +1,24 @@
 package ro.pao.service;
 
+import ro.pao.model.Librarian;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface LibrarianService {
+    Optional<Librarian> getLibrarianById(UUID id);
+
+    Optional<Librarian> getLibrarianByUserName(String userName);
+
+    List<Librarian> getAllLibrarians();
+
+    void addLibrarian(Librarian librarian);
+
+    void addLibrarians(List<Librarian> librariansList);
+
+    void deleteLibrarianById(UUID id);
+
+    void deleteLibrarianByUserName(String userName);
 
 }
--- src/main/java/ro/pao/service/impl/LibrarianServiceImpl.java
+++ src/main/java/ro/pao/service/impl/LibrarianServiceImpl.java
@@ -1,7 +1,62 @@
 package ro.pao.service.impl;
 
+import ro.pao.model.Librarian;
 import ro.pao.service.LibrarianService;
 
+import java.util.*;
+
 public class LibrarianServiceImpl implements LibrarianService {
+    private Set<Librarian> librarians = new HashSet<>();
+
+    @Override
+    public Optional<Librarian> getLibrarianById(UUID id){
+        return librarians
+                .stream()
+                .filter(librarian -> librarian.getId().equals(id))
+                .findFirst();
+    }
+
+    @Override
+    public Optional<Librarian> getLibrarianByUserName(String userName){
+        return librarians
+                .stream()
+                .filter(librarian -> librarian.getUserName().equals(userName))
+                .findFirst();
+    }
+
+    @Override
+    public List<Librarian> getAllLibrarians(){
+        return new ArrayList<>(librarians);
+    }
+
+    @Override
+    public void addLibrarian(Librarian librarian){
+        librarians.add(librarian);
+    }
+
+    @Override
+    public void addLibrarians(List<Librarian> librariansList){
+        librarians.addAll(librariansList);
+    }
+
+    @Override
+    public void deleteLibrarianById(UUID id){
+        librarians
+                .stream()
+                .filter(librarian -> librarian.getId().equals(id))
+                .findFirst()
+                .ifPresent(librarians::remove);
+    }
+
+    @Override
+    public void deleteLibrarianByUserName(String userName){
+        librarians
+                .stream()
+                .filter(librarian -> librarian.getUserName().equals(userName))
+                .findFirst()
+                .ifPresent(librarians::remove);
+    }
+
+
 
 }
